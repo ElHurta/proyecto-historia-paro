ab6ce4c41a4a24ebd257f6dd0e6e7dafa9c33dcb
ElHurta <judhurtadop@correo.udistrital.edu.co>
2021-05-30 16:05:08 -0500

    Agregada generación de secciones por JS, definido layout inicial para cada sección, agregadas keys requeridas en JSON, cambios en algunas imágenes realizados

--- scripts/renderTopics.js
+++ scripts/renderTopics.js
@@ -1,14 +1,34 @@
 var left_codigo = ""
 var right_codigo = ""
 var codigo = ""
+var sec_code = ""
 const topics_left = document.querySelector(".left-topics")
 const topics_right = document.querySelector(".right-topics")
 const topics = document.querySelector(".topics-sidebar")
+const content = document.querySelector(".content")
 
 
-function generarCodigoTopic(año, acon) {
+function generarCodigoTopic(ahno, acon) {
     return `
-    <li><a href="#${año}">${año} - ${acon}</a></li>
+    <li><a href="#${ahno}">${ahno} - ${acon}</a></li>
+    `
+}
+
+function generarCodigoSecc(anho, acon, resum, imageUrl) {
+    return `
+    <div class="acon" id="${anho}" style="background-image: url('../assets/images/Textura1.png');">
+        <div class="img-container">
+          <img src="${imageUrl}" alt="Universidad Distrital" width="90%" height="90%"/>
+        </div>
+        <div class="desc">
+            <h3>
+                ${acon} - ${anho}
+            </h3>
+            <p>
+                ${resum}
+            </p>
+        </div>
+      </div>
     `
 }
 
@@ -23,13 +43,16 @@ function renderTopics(data) {
         } else if (i >= mitad) {
             right_codigo += generarCodigoTopic(anho, acon)
         }
+
+        sec_code += generarCodigoSecc(anho, acon, topics[i].resumen, topics[i].imageUrl)
     }
-    console.log(topics_left)
-    console.log(left_codigo)
+    //console.log(topics_left)
+    //console.log(left_codigo)
     topics_left.innerHTML = left_codigo
-    console.log(topics_right)
-    console.log(right_codigo)
+    //console.log(topics_right)
+    //console.log(right_codigo)
     topics_right.innerHTML = right_codigo
+    content.innerHTML = sec_code
 }
 
 function renderSidebar(data) {
